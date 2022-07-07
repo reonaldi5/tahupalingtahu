045a3fb307fd5b6cd20031d02a30ce10e5fcc6b2
reonaldi5 <reonaldi152@gmail.com>
2022-07-07 13:29:03 +0700

    detail

--- resources/views/transactions/detail.blade.php
+++ resources/views/transactions/detail.blade.php
@@ -12,57 +12,58 @@
                     <div class="w-full md:w-1/6 px-4 mb-4 md:mb-0">
                         <img src="{{ $item->food->picturePath }}" alt="" class=" bg-[length:100px_50px]">
                     </div>
+                    <br><br><br>
                     <div class="w-full md:w-5/6 px-4 mb-4 md:mb-0">
                         <div class="flex flex-wrap mb-3">
                             <div class="w-2/6">
                                 <div class="text-sm">Product Name</div>
                                 <div class="text-xl font-bold">{{ $item->food->name }}</div>
                             </div>
-                            <br><br>
+                            <br><br><br>
                             <div class="w-1/6">
                                 <div class="text-sm">Quantity</div>
                                 <div class="text-xl font-bold">{{ number_format($item->quantity) }}</div>
                             </div>
-                            <br>
+                            <br><br><br>
                             <div class="w-2/6">
                                 <div class="text-sm">Total</div>
                                 <div class="text-xl font-bold">{{ number_format($item->total) }}</div>
                             </div>
-                            <br>
+                            <br><br><br>
                             <div class="w-1/6">
                                 <div class="text-sm">Status</div>
                                 <div class="text-xl font-bold">{{ $item->status }}</div>
                             </div>
                         </div>
-                        <br>
+                        <br><br><br>
                         <div class="flex flex-wrap mb-3">
                             <div class="w-2/6">
                                 <div class="text-sm">User Name</div>
                                 <div class="text-xl font-bold">{{ $item->user->name }}</div>
                             </div>
-                            <br>
+                            <br><br><br>
                             <div class="w-3/6">
                                 <div class="text-sm">Email</div>
                                 <div class="text-xl font-bold">{{ $item->user->email }}</div>
                             </div>
-                            <br>
+                            <br><br><br>
                             <div class="w-1/6">
                                 <div class="text-sm">City</div>
                                 <div class="text-xl font-bold">{{ $item->user->city }}</div>
                             </div>
                         </div>
-                        <br>
+                        <br><br><br>
                         <div class="flex flex-wrap mb-3">
                             <div class="w-4/6">
                                 <div class="text-sm">Address</div>
                                 <div class="text-xl font-bold">{{ $item->user->address }}</div>
                             </div>
-                            <br>
+                            <br><br><br>
                             <div class="w-1/6">
                                 <div class="text-sm">Number</div>
                                 <div class="text-xl font-bold">{{ $item->user->houseNumber }}</div>
                             </div>
-                            <br>
+                            <br><br><br>
                             <div class="w-1/6">
                                 <div class="text-sm">Phone</div>
                                 <div class="text-xl font-bold">{{ $item->user->phoneNumber }}</div>
@@ -92,15 +93,19 @@
                                 <a style="background-color: blue;" href="{{ route('transactions.changeStatus', ['id' => $item->id, 'status' => 'belum_dibayar']) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 rounded block text-center w-full mb-1">
                                     Belum Dibayar
                                 </a>
+                                <br>
                                 <a style="background-color: green;" href="{{ route('transactions.changeStatus', ['id' => $item->id, 'status' => 'sudah_dibayar']) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold px-2 rounded block text-center w-full mb-1">
                                     Sudah Dibayar
                                 </a>
+                                <br>
                                 <a style="background-color: green;" href="{{ route('transactions.changeStatus', ['id' => $item->id, 'status' => 'sedang_perjalanan']) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold px-2 rounded block text-center w-full mb-1">
                                     Sedang Perjalanan
                                 </a>
+                                <br>
                                 <a style="background-color: green;" href="{{ route('transactions.changeStatus', ['id' => $item->id, 'status' => 'sudah_tiba']) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold px-2 rounded block text-center w-full mb-1">
                                     sudah Tiba
                                 </a>
+                                <br>
                                 <a style="background-color: red;" href="{{ route('transactions.changeStatus', ['id' => $item->id, 'status' => 'CANCELLED']) }}" class="bg-red-500 hover:bg-red-700 text-white font-bold px-2 rounded block text-center w-full mb-1">
                                     Cancelled
                                 </a>
