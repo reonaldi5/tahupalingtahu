8741e269a86c86c9e42b024aae568539c902ca7b
reonaldi5 <reonaldi152@gmail.com>
2022-08-16 10:46:09 +0700

    stok5

--- resources/views/produk/create.blade.php
+++ resources/views/produk/create.blade.php
@@ -67,7 +67,7 @@
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                                 Stok
                             </label>
-                            <input value="{{ old('Stok') }}" name="Stok" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="produk Stok">
+                            <input value="{{ old('Stok') }}" name="Stok" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="produk Stok">
 
                         </div>
                         <br>
